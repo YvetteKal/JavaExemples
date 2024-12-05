d0cc473bddf730e7c0e8c95ea80f815f8a37235d
YvetteKal <yvettekalimumbalo@gmail.com>
2024-12-05 12:43:17 +0300

    changer numeroTaxi to couleur

--- Taxi.java
+++ Taxi.java
@@ -1,12 +1,12 @@
 public class Taxi extends Voiture {
 
-    private int numeroTaxi;
+    private String couleur;
     private String itineraire;
 
     //constructeur de la classe fille
-    public Taxi(String numero, String marque, int nombrePortes, int nombrePneus, int nombreChaises, int numeroTaxi, String itineraire){
+    public Taxi(String numero, String marque, int nombrePortes, int nombrePneus, int nombreChaises, String couleur, String itineraire){
         super(numero, marque, nombrePortes, nombrePneus, nombreChaises);
-        this.numeroTaxi = numeroTaxi;
+        this.couleur = couleur;
         this.itineraire = itineraire;
     }
 }
