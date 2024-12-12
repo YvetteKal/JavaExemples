f0bc0d4affc32aca9e14d36c50f2ed3e2be7c7b3
YvetteKal <yvettekalimumbalo@gmail.com>
2024-12-12 16:06:59 +0300

    equation second degre corrige

---
+++ EquationSecondDegre.java
@@ -0,0 +1,48 @@
+
+public class EquationSecondDegre {
+    // fonction pour calculer delta
+    public static double calculDelta(double ca, double cb, double cc){
+        //delta = b2 -4ac
+        double delta = (Math.pow(2, cb)) - 4*(ca*cc);
+        // Afficher le discriminant
+        System.out.println("Le discriminant (Δ) est : " + delta);
+
+        return delta;
+    }
+
+    //procedureafficher les solutions
+    public static void afficherResultats(double ca, double cb, double delta){
+        //calcul des racines
+        //x1,x2 = -b+-sqrt (delta)/2b
+        double x1, x2;
+
+        if (delta > 0){
+
+            x1 = (-cb + Math.sqrt(delta))/(2*ca);
+            x2 = (-cb - Math.sqrt(delta))/(2*ca);
+            System.out.println("Les racines sont : x1 = " + x1 + " et x2 = " + x2);
+
+        }else if (delta == 0){
+            //une recine reelle double
+            x1 = -cb/(2*ca);
+            System.out.println("La racine double est : x1 = x2 = " + x1);
+
+        }else{
+            //racines complexes
+            // double i = Math.sqrt(-1);
+            // x1 = (-cb + i * Math.sqrt(-delta))/(2*ca);
+            // x2 = (-cb - i * Math.sqrt(-delta))/(2*ca);
+            // System.out.println("Les recines imaginaires sont: x1 = " + x1 + " et x2 = " + x2);
+            // Racines complexes
+            double realPart = -cb / (2 * ca);
+            double imaginaryPart = Math.sqrt(-delta) / (2 * ca);
+            System.out.println("Les racines complexes sont : x1 = " + realPart + " - " + imaginaryPart + 
+            "i et x2 = " + realPart + " + " + imaginaryPart + "i");
+
+
+        }
+    }
+
+    
+
+}
---
+++ Exercice5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Exercice5 {
+
+    public static void main(String[] args) {
+        //Suite exercices TD, Question 6
+        //resolution ax2 + bx + c = 0
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer le coefficient a (non nul)");
+        double ca = sc.nextInt(); //ca = coefficient a
+
+        System.out.println("Entrer le coefficient b");
+        double cb = sc.nextInt();
+
+        System.out.println("Entrer le coefficient c");
+        double cc = sc.nextInt();
+
+        //afficher l'equatio a resoudre
+        System.out.println("L'equation à resoudre est: "+ca+ "x2 + "+cb+ "x + "+cc);
+
+        //calcul de delta
+        double delta = EquationSecondDegre.calculDelta(ca, cb, cc);
+
+        EquationSecondDegre.afficherResultats(ca, cb, delta);            
+     
+    }
+    
+
+}
